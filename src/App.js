d5183489697c8feae218bd3f47c141781c187d09
Ankita Kumari <ankita.ashreet@gmail.com>
2023-09-03 14:04:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import SideBar from "./pages/SideBar";
+import Main from "./pages/Main";
+import { data } from "./utility/data";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const onChangePage = (pageName) => {
+    setCurrentPage(pageName);
+  };
+  const handleChange = (term) => {
+    setSearchTerm(term);
+  };
+  let renderedData = data;
+  if (currentPage === "Polygon") {
+    renderedData = renderedData.slice(0, 11);
+  } else if (currentPage === "Arrow") {
+    renderedData = renderedData.filter((each) => {
+      return each.name.includes(currentPage);
+    });
+  } else if (currentPage === "Circle") {
+    renderedData = renderedData.filter((each) => {
+      return each.name.includes(currentPage) || each.name.includes("Ellipse");
+    });
+  } else if (currentPage === "Miscellaneous") {
+    renderedData = renderedData.slice(11).filter((each) => {
+      return (
+        !each.name.includes("Arrow") &&
+        !each.name.includes("Circle") &&
+        !each.name.includes("Ellipse")
+      );
+    });
+  }
+
+  renderedData = renderedData.filter((each) => {
+    return each.name.toLowerCase().includes(searchTerm.trim().toLowerCase());
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app md:flex'>
+      <SideBar
+        onChange={handleChange}
+        searchTerm={searchTerm}
+        onChangePage={onChangePage}
+        currentPage={currentPage}
+      />
+
+      <Main data={renderedData} searchTerm={searchTerm} />
     </div>
   );
 }
